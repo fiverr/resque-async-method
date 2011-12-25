e2cfdc307e683b7ba631eae24697a0bf7f8cc0c9
Tom Caspy <tom@fiverr.com>
2011-12-25 19:35:52 +0200

    added documentation

--- lib/resque/plugins/async/method.rb
+++ lib/resque/plugins/async/method.rb
@@ -8,22 +8,37 @@ module Resque::Plugins::Async::Method
     my_klass.queue = opts[:queue] ||
                      send(:class).name.underscore.pluralize
     if self.methods(false).include?(method.to_s)
-      #class method
+      # this means that the method called is a class method
       id = 0
     else
-      #instance, thus there is an ID
+      # instance, thus there is an ID
       id = send(:id)
     end
+
+    # methods which have an ! in their end, need to move the ! to the end, this creates
+    # some rather ugly code, but what we do is we clean the !s, write the method's name,
+    # and then check that it did have a ! in the end, finishing the new method name with a bang.
+    
     clean_method = method.to_s.gsub("!","")
     method_without_enqueue = "#{clean_method}_without_enqueue#{'!' if clean_method != method.to_s}"
     
-    *args = *args.map do |object|
-      if object.is_a?(ActiveRecord::Base)
-        {:class_name => object.class.name, :id => object.id}
+
+    # resque's stupid way of handling ActiveRecord parameters turns the parameter into a hash
+    # workaround this issue is to pass them as a hash, and turn them back into their source
+    # in the worker (see worker.rb)
+    # issue is here: http://librelist.com/browser/resque/2010/5/20/passing-activerecord-arguments-to-jobs/
+    # workaround: https://github.com/zapnap/resque_mailer/commit/9a0ad10
+
+    *args = *args.map do |argument|
+      if argument.is_a?(ActiveRecord::Base)
+        {:class_name => argument.class.name, :id => argument.id}
       else
-        object
+        argument
       end
     end
+
+    # The enqueu uses the Async plugin class, which allows setting the queue name in the
+    # async_method call, a-la delayed_job
 
     Resque.enqueue(
       my_klass,
@@ -32,16 +47,19 @@ module Resque::Plugins::Async::Method
       :"#{method_without_enqueue}",
       *args
     )
-    #Delayed::Job.enqueue Delayed::PerformableMethod.new(self, method.to_sym, args)
   end
   
   module ClassMethods
+
     def async_method(method, opts={})
+      #we clean the method, please see the documentation above
+      
       clean_method = method.to_s.gsub("!","")
       define_method("#{clean_method}_with_enqueue#{'!' if clean_method != method.to_s}") do |*args|
         enqueue(method, opts, *args)
       end
-      alias_method_chain method, :enqueue
+      ali
+      as_method_chain method, :enqueue
     end
   end
 
--- lib/resque/plugins/async/worker.rb
+++ lib/resque/plugins/async/worker.rb
@@ -9,14 +9,22 @@ class Resque::Plugins::Async::Worker
     @queue
   end
   
+  # we want to make sure that the objects passed to the worker make sense
+  # resque serializes AR arguments, making calls fail miserably
+  # thus, we turned them into hashes which look like {:class_name => Class, :id => id}
+  # and turn them back to AR records before using them.
+
   def self.perform(klass, *args)
     id = args.shift
+    # the first argument is always the method name.
+    
     arguments = *args.map { |o| o.is_a?(Hash) && o.has_key?("class_name") && o.has_key?("id") ? o["class_name"].constantize.find(o["id"]) : o }
+    
     if id == 0
-      #class method
+      # id = 0 is the de-facto way fo saying "This is a class method"
       klass.constantize.send(arguments.shift, *arguments)
     else
-      #instance method
+      # instance method
       klass.constantize.find(id).send(arguments.shift, *arguments)
     end
   end
