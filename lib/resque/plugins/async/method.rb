f93c43d14faf0c9a4fae73b7b46681b1bcc27785
Tom Caspy <tom@fiverr.com>
2011-12-25 16:56:48 +0200

    [FEATURE] thank you DJ, you've done an awesome job with your code, now we steal it :)

--- lib/resque/plugins/async/method.rb
+++ lib/resque/plugins/async/method.rb
@@ -3,43 +3,37 @@ require 'resque/plugins/async/worker'
 module Resque::Plugins::Async::Method
   extend ActiveSupport::Concern
 
+  def enqueue(method, opts, *args)
+    my_klass       = Resque::Plugins::Async::Worker
+    my_klass.queue = opts[:queue] ||
+                     send(:class).name.underscore.pluralize
+    if self.methods(false).include?(method.to_s)
+      #class method
+      id = 0
+    else
+      #instance, thus there is an ID
+      id = send(:id)
+    end
+    clean_method = method.to_s.gsub("!","")
+    method_without_enqueue = "#{clean_method}_without_enqueue#{'!' if clean_method != method.to_s}"
+    Resque.enqueue(
+      my_klass,
+      send(:class).name == "Class" ? send(:name) : send(:class).name,
+      id,
+      :"#{method_without_enqueue}",
+      *args
+    )
+    #Delayed::Job.enqueue Delayed::PerformableMethod.new(self, method.to_sym, args)
+  end
+  
   module ClassMethods
-    def async_method(method_name, opts={})
-      # Allow tests to call sync_ methods ...
-      alias_method :"sync_#{method_name}", method_name
-    
-      # ... but don't actually make them asynchronous
-      return if Rails.env.test?
-
-      if self.methods(false).include?(method_name.to_s)
-        #class method
-        id = 0
-      else
-        #instance, thus there is an ID
-        id = send(:id)
-      end    
-      define_method "#{method_name}" do |*args|
-        my_klass       = Resque::Plugins::Async::Worker
-        my_klass.queue = opts[:queue] ||
-                         send(:class).name.underscore.pluralize
-        if self.new_record?
-          Resque.enqueue(
-            my_klass,
-            send(:class).name,
-            0,
-            :"sync_#{method_name}",
-            *args
-          )
-        else
-          Resque.enqueue(
-            my_klass,
-            send(:class).name,
-            id,
-            :"sync_#{method_name}",
-            *args
-          )
-        end
+    def async_method(method, opts={})
+      clean_method = method.to_s.gsub("!","")
+      define_method("#{clean_method}_with_enqueue#{'!' if clean_method != method.to_s}") do |*args|
+        enqueue(method, opts, *args)
       end
+      alias_method_chain method, :enqueue
     end
   end
+
 end
