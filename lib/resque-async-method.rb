ae345d44c3b35ab61b8be8d53857f4e5a01ef719
Moshe Lieberman <moshe@fiverr.com>
2013-03-14 11:45:33 +0200

    [FEAT] adding class worker to auto load

--- lib/resque-async-method.rb
+++ lib/resque-async-method.rb
@@ -3,8 +3,9 @@ require 'active_support/dependencies'
 module Resque
   module Plugins
     module Async
-      autoload :Method, 'resque/plugins/async/method'
+      autoload :Method, 	'resque/plugins/async/method'
       autoload :Worker, 'resque/plugins/async/worker'
+      autoload :ClassWorker, 'resque/plugins/async/class_worker'
     end
   end
 end
